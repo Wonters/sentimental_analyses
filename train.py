1c03dcabd0f16a9d3ef648e3c33a33b483203597
shift <shift.python.software@gmail.com>
2025-04-22 19:34:08 +0200

    grafana alerts

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 ## Import lightgbm avoiding segfault error, protection against segfault
 #import lightgbm as lgb
 from src.ml import LightGBMModel, load_data, LSTMModel
+import logging
 
 df  = load_data('../data/training.1600000.processed.noemoticon.csv')
 #df = df.sample(frac=1, random_state=42)
