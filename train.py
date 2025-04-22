a56fb4091ea7753fd3790e2a8de1d045dfd3ed99
shift.python.software@gmail.com <shift.python.software@gmail.com>
2025-04-22 17:09:31 +0000

    distribute

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 ## Import lightgbm avoiding segfault error, protection against segfault
 #import lightgbm as lgb
+import logging
 from src.ml import LightGBMModel, load_data, LSTMModel
 
+logging.basicConfig(level=logging.INFO)
+
 df  = load_data('../data/training.1600000.processed.noemoticon.csv')
-#df = df.sample(frac=1, random_state=42)
+df = df.sample(frac=0.1, random_state=42)
 model = LSTMModel(df)
 model.train()
